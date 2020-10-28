3dbab9bc96a63a5e2ea9f522470ca33db4a54135
shaishav <shaishav.shah.cs@gmail.com>
2020-10-28 18:03:52 -0400

    Add basic main file accepting and validating inputs

---
+++ src/Main.fsx
@@ -0,0 +1,20 @@
+#load @"./Utils.fsx"
+
+#r "nuget: Akka.FSharp"
+
+open Utils
+
+let isValidInput (numberOfNodes, numberOfRequests) =
+    (numberOfNodes > 0) && (numberOfRequests > 0)
+
+let main (numberOfNodes, numberOfRequests) = 
+    if not (isValidInput (numberOfNodes, numberOfRequests)) then
+        printfn "Error: Invalid Input"
+    else 
+        printfn "Done!"
+
+// Read command line inputs and pass on to the driver function
+match fsi.CommandLineArgs with
+    | [|_; numberOfNodes; numberOfRequests|] -> 
+        main ((Utils.strToInt numberOfNodes), (Utils.strToInt numberOfNodes))
+    | _ -> printfn "Error: Invalid Arguments"

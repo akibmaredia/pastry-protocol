79eb044b8448273acf3399734a68376ded3c6006
shaishav <shaishav.shah.cs@gmail.com>
2020-11-03 00:46:23 -0500

    Add int to base string conversion function

--- src/Utils.fsx
+++ src/Utils.fsx
@@ -16,3 +16,24 @@ module Utils =
     // x^y
     let powOf (x: int, y: int) = 
         Math.Pow (toDouble x, toDouble y)
+
+    let intToDigits baseVal number : int list = 
+        let rec loop value digitArr = 
+            let quotient = value / baseVal
+            let remainder = value % baseVal
+            if quotient = 0 then remainder :: digitArr
+            else loop quotient (remainder :: digitArr)
+        loop number []
+
+    let numToBase(number: int, length: int, baseVal: int) = 
+        let mutable result: string = 
+            number
+            |> intToDigits baseVal
+            |> List.fold (fun acc x ->  acc + x.ToString()) ""
+        
+        let numZeroesToAppend = length - (String.length result)
+        if numZeroesToAppend > 0 then
+            let zeroesStr = String.init numZeroesToAppend (fun i ->   "0")
+            result <- zeroesStr + result
+        
+        result
